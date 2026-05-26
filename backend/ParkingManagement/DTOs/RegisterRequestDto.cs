e7c7e81c22f70d60a3524cf1c5ad733ec8b6e9c8
Khang <khanghuynh.business@gmail.com>
2026-05-26 14:32:57 +0700

    Register for user

---
+++ backend/ParkingManagement/DTOs/RegisterRequestDto.cs
@@ -0,0 +1,17 @@
+﻿namespace ParkingManagement.DTOs
+{
+    public class RegisterRequestDto
+    {
+        public string FullName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string ConfirmPassword { get; set; } = null!;
+    }
+
+    public class UpdateUserNameDto
+    {
+        public string UserId { get; set; } = null!;
+        public string NewUsername { get; set; } = null!;
+    }
+}
---
+++ backend/ParkingManagement/Utils/ValidationUtils.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ParkingManagement.Utils
+{
+    public static class ValidationUtils
+    {
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email)) return false;
+            string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            return Regex.IsMatch(email, emailPattern);
+        }
+
+        public static bool IsValidPhoneNumber(string phoneNumber)
+        {
+            if (string.IsNullOrEmpty(phoneNumber)) return false;
+            string phonePattern = @"^(0[3|5|7|8|9])+([0-9]{8})$";               // Điện thoại có 10 chữ số và dùng cho Việt Nam
+            return Regex.IsMatch(phoneNumber, phonePattern);
+        }
+
+        public static bool IsValidPassword(string password)
+        {
+            if (string.IsNullOrEmpty(password)) return false;
+            string passwordPattern = @"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$"; // Mật khẩu tối thiểu 6 ký tự, ít nhất một chữ hoa và một số
+            return Regex.IsMatch(password, passwordPattern);
+        }
+
+        public static bool IsValidUsername(string username)
+        {
+            if (string.IsNullOrEmpty(username)) return false;
+            string usernamePattern = @"^[a-zA-Z0-9._-]{4,20}$";                 // Username tối thiểu 3 ký tự, chỉ chứa chữ cái, số, dấu chấm, gạch dưới hoặc gạch ngang
+            return Regex.IsMatch(username, usernamePattern);
+        }
+    }
+}
+
